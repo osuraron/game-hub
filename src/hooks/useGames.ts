e2e94e1e07c825edb227274fe0b113a1a3cb885b
osura <osura.ronath@gmail.com>
2023-07-27 00:31:51 +0530

    create custom hook

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import apiClient from '../services/api-client';
+
+
+interface Games {
+    id: number;
+    name: string;
+  }
+
+interface FetchGamesResponse {
+    count: number;
+    results: Games[];
+  }
+
+const useGames = () => {
+    const [games, setGames] = useState<Games[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+        const controller = new AbortController()
+      apiClient
+        .get<FetchGamesResponse>("/games", {signal: controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if(err instanceof CanceledError) return;
+            setError(err.message)});
+
+        return () => controller.abort()
+    },[]);
+//[] is stop constant requests
+    return {games, error}
+}
+
+export default useGames
