1526a14e52a7e51444e01ecd8a6eafdf8af30574
osura <osura.ronath@gmail.com>
2023-07-26 23:56:50 +0530

    fetch games

---
+++ src/services/api-client.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: "195edf1e498847979af4572e397834ce"
+    }
+})
+
+
+
+
